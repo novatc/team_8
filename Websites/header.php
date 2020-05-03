1a9e421172c44b168c8a8128bcee9d104b0d4a1e
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2020-05-03 17:21:50 +0200

    test

---
+++ Websites/header.php
@@ -0,0 +1,12 @@
+
+<header>
+    <nav>
+        <p><a href="/home.html">Home</a> | 
+            <a href="Teammates/gameoverview.html">Spiele</a> | 
+            <input type="search" id="site-search" style="float: center" ><button style="float: center">Suche</button>
+            <a href="User/login.html">Login</a> |  
+            <a href="User/registration.html">Registrieren</a> | 
+            <a href="User/playerprofil.html">Profil</a></p>
+
+    </nav>
+</header>
