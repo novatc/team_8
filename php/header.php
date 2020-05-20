2be922afc1bbda4986006355f86501bc0863ba85
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2020-05-20 16:43:12 +0200

    little header fix

--- php/header.php
+++ php/header.php
@@ -3,18 +3,17 @@
     <div class="grid-container">
         <div class="main-nav">
             <a class = "home-link" href="index.php">Home</a>
-            <a class = "game-link"href="gameoverview.php">Spiele</a>
+            <a class = "game-link" href="gameoverview.php">Spiele</a>
         </div>
         <div class="search-wrapper">
             <img class= search-icon src="Resourcen/Navigation/search-black-24dp.svg">
             <input class="search" type="text" placeholder="Suche.." name="search">
         </div> 
         <div class="profil-nav">
-            <a class = "login-link"href="login.php">Login</a>
-            <a class = "register-link"href="registration.php">Registrieren</a>
-            <a class = "message-link"href="chatoverview.php">Nachrichten</a>
-            <a class = "profil-link"href="playerprofil.php">Profil</a>
-            
+            <a class = "login-link" href="login.php">Login</a>
+            <a class = "register-link" href="registration.php">Registrieren</a>
+            <a class = "message-link" href="chatoverview.php">Nachrichten</a>
+            <a class = "profil-link" href="playerprofil.php">Profil</a>  
         </div>
     </div>
 </nav>
