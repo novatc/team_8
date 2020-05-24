87c9d10bc3e3d5d84621e35abce8e8fe176ca1cb
Lucas <lucas.wagner@uol.de>
2020-05-24 15:19:37 +0200

    css fpr startpage

--- index.php
+++ index.php
@@ -4,20 +4,41 @@
     <meta charset="UTF-8">
     <title>Team8</title>
     <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/start.css">
+    <link rel="stylesheet" type="text/css" href="css/games.css">
 </head>
 <body>
-    <header>
-        <div class="mainnav">
-            <?php include "php/header.php";?>
+<header>
+    <div class="mainnav">
+        <?php include "php/header.php"; ?>
+    </div>
+</header>
+
+<main>
+    <div class="wrapper">
+        <div class="split left">
+            <div class="centered">
+                <h2>Login</h2>
+                <p>Some text.</p>
+            </div>
         </div>
-    </header>
-    <main>
-        Wilkommen bei Team8
-    </main>
-    <footer>
-        <div class="footer">
-            <?php include "php/footer.php";?>
+
+        <div class="split right">
+            <div class="centered">
+                <h2>Registrieren</h2>
+                <p>Some text here too.</p>
+            </div>
         </div>
-    </footer>
+    </div>
+
+
+
+</main>
+
+<footer>
+    <div class="footer">
+        <?php include "php/footer.php"; ?>
+    </div>
+</footer>
 </body>
 </html>
