8f4a31c35cd03772df73c79b4eb5bb313ee4cb21
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2020-07-08 18:08:17 +0200

    footer + Impressum + Datenschutzerklärung angepasst

--- about.php
+++ about.php
@@ -19,7 +19,7 @@
 <main>
     <h1>Über uns</h1>
     <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        Team8 ist eine Unternehmen mit der Vision Gamer zu verbinden. 
     </p>
 
 
--- impressum.php
+++ impressum.php
@@ -17,39 +17,49 @@
     </div>
 </header>
 <main>
-    <h1 ">Impressum</h1>
-    <p ">Informationspflicht laut § 5 TMG.</p>
-    <p ">
-    <p>Lucas Wagner, Nico Schönfisch, Johannes Scheibe</p>
-    <p ">Ammerländer Herrstr.114-118, <br />26129 Oldenburg, <br />Deutschland</p>
-    <p ">
-    <strong>Tel.:</strong> +49 441 008<br />
-    <strong>Fax:</strong> +49 441 007<br />
+    <h1>Impressum</h1>
+    <p>Informationspflicht laut § 5 TMG.</p>
+    <p>Team8<br />Amerländer Herrstraße 114-118, <br />26129 Oldenburg, <br />Deutschland</p>
+    <p>
+    <strong>UID-Nummer:</strong> DE12345678<br />
+    <strong>Wirtschafts-ID:</strong> DE123456789<br />
+    </p>
+    <p>
+    <strong>Tel.:</strong> 0441/83268<br />
+    <strong>Fax:</strong> 0441/83268<br />
     <strong>E-Mail:</strong> <a href="mailto:office@team8.de">office@team8.de</a>
     </p>
-    <p style="margin-top:15px;">Quelle: Erstellt mit dem <a title="Impressum Generator Deutschland" href="https://www.adsimple.de/impressum-generator/" target="_blank" rel="follow" style="text-decoration:none;">Impressum Generator</a> von AdSimple in Kooperation mit <a href="https://www.hashtagbeauty.de" target="_blank" rel="follow" title="">hashtagbeauty.de</a>
+    <p>
+    <strong>Aufsichtsbehörde</strong>
+    <br> Die Landesbeauftragte für den Datenschutz Niedersachsen<br />
+    <strong>Webseite der Aufsichtsbehörde</strong>
+    <br>
+    <a href="https://lfd.niedersachsen.de" target="_blank">https://lfd.niedersachsen.de</a>
+    <br />
+    <strong>Anschrift der Aufsichtsbehörde</strong>
+    <br> Prinzenstr. 5, 30159 Hannover<br />
     </p>
-    <h2 ">EU-Streitschlichtung</h2>
+    <p>
+    <strong>Geschäftsführer</strong>
+    <br />Lucas Wagner, Nico Schönfisch, Johannes Scheibe</p>
+    <p style="margin-top:15px;">Quelle: Erstellt mit dem <a title="Impressum Generator Deutschland" href="https://www.adsimple.de/impressum-generator/" target="_blank" rel="follow" style="text-decoration:none;">Impressum Generator</a> von AdSimple in Kooperation mit <a href="https://www.justmed.de" target="_blank" rel="follow" title="">justmed.de</a>
+    </p>
+    <h2>EU-Streitschlichtung</h2>
     <p>Gemäß Verordnung über Online-Streitbeilegung in Verbraucherangelegenheiten (ODR-Verordnung) möchten wir Sie über die Online-Streitbeilegungsplattform (OS-Plattform) informieren.<br />
-    Verbraucher haben die Möglichkeit, Beschwerden an die Online Streitbeilegungsplattform der Europäischen Kommission unter <a " href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&amp;lng=DE" target="_blank" rel="noopener">http://ec.europa.eu/odr?tid=321191315</a> zu richten. Die dafür notwendigen Kontaktdaten finden Sie oberhalb in unserem Impressum.</p>
+    Verbraucher haben die Möglichkeit, Beschwerden an die Online Streitbeilegungsplattform der Europäischen Kommission unter <a href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&amp;lng=DE" target="_blank" rel="noopener">http://ec.europa.eu/odr?tid=321192685</a> zu richten. Die dafür notwendigen Kontaktdaten finden Sie oberhalb in unserem Impressum.</p>
     <p>Wir möchten Sie jedoch darauf hinweisen, dass wir nicht bereit oder verpflichtet sind, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-    <h2 ">Haftung für Inhalte dieser Website</h2>
-    <p>Wir entwickeln die Inhalte dieser Webseite ständig weiter und bemühen uns korrekte und aktuelle Informationen bereitzustellen. Laut Telemediengesetz <a " href="https://www.gesetze-im-internet.de/tmg/__7.html?tid=321191315" rel="noopener" target="_blank">(TMG) §7 (1)</a> sind wir als Diensteanbieter für eigene Informationen, die wir zur Nutzung bereitstellen, nach den allgemeinen Gesetzen verantwortlich. Leider können wir keine Haftung für die Korrektheit aller Inhalte auf dieser Webseite übernehmen, speziell für jene die seitens Dritter bereitgestellt wurden. Als Diensteanbieter im Sinne der §§ 8 bis 10 sind wir nicht verpflichtet, die von ihnen übermittelten oder gespeicherten Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+    <h2>Haftung für Inhalte dieser Website</h2>
+    <p>Wir entwickeln die Inhalte dieser Webseite ständig weiter und bemühen uns korrekte und aktuelle Informationen bereitzustellen. Laut Telemediengesetz <a href="https://www.gesetze-im-internet.de/tmg/__7.html?tid=321192685" rel="noopener" target="_blank">(TMG) §7 (1)</a> sind wir als Diensteanbieter für eigene Informationen, die wir zur Nutzung bereitstellen, nach den allgemeinen Gesetzen verantwortlich. Leider können wir keine Haftung für die Korrektheit aller Inhalte auf dieser Webseite übernehmen, speziell für jene die seitens Dritter bereitgestellt wurden. Als Diensteanbieter im Sinne der §§ 8 bis 10 sind wir nicht verpflichtet, die von ihnen übermittelten oder gespeicherten Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
     <p>Unsere Verpflichtungen zur Entfernung von Informationen oder zur Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen aufgrund von gerichtlichen oder behördlichen Anordnungen bleiben auch im Falle unserer Nichtverantwortlichkeit nach den §§ 8 bis 10 unberührt. </p>
     <p>Sollten Ihnen problematische oder rechtswidrige Inhalte auffallen, bitte wir Sie uns umgehend zu kontaktieren, damit wir die rechtswidrigen Inhalte entfernen können. Sie finden die Kontaktdaten im Impressum.</p>
-    <h2 ">Haftung für Links auf dieser Website</h2>
+    <h2>Haftung für Links auf dieser Website</h2>
     <p>Unsere Webseite enthält Links zu anderen Webseiten für deren Inhalt wir nicht verantwortlich sind. Haftung für verlinkte Websites besteht für uns nicht, da wir keine Kenntnis rechtswidriger Tätigkeiten hatten und haben, uns solche Rechtswidrigkeiten auch bisher nicht aufgefallen sind und wir Links sofort entfernen würden, wenn uns Rechtswidrigkeiten bekannt werden.</p>
     <p>Wenn Ihnen rechtswidrige Links auf unserer Website auffallen, bitte wir Sie uns zu kontaktieren. Sie finden die Kontaktdaten im Impressum.</p>
-    <h2 ">Urheberrechtshinweis</h2>
+    <h2>Urheberrechtshinweis</h2>
     <p>Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen dem Urheberrecht der Bundesrepublik Deutschland. Bitte fragen Sie uns bevor Sie die Inhalte dieser Website verbreiten, vervielfältigen oder verwerten wie zum Beispiel auf anderen Websites erneut veröffentlichen. Falls notwendig, werden wir die unerlaubte Nutzung von Teilen der Inhalte unserer Seite rechtlich verfolgen.</p>
     <p>Sollten Sie auf dieser Webseite Inhalte finden, die das Urheberrecht verletzen, bitten wir Sie uns zu kontaktieren.</p>
-    <h2 ">Bildernachweis</h2>
+    <h2>Bildernachweis</h2>
     <p>Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich geschützt.</p>
-    <p>Die Bilderrechte liegen bei den folgenden Fotografen und Unternehmen:</p>
-    <ul ">
-    <li ">Fotograf Mustermann</li>
-    </ul>
-    
 </main>
 
 
