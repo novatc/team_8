069e6ff8b032b6a5b6a6969a5323c53d436c09ce
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2020-07-25 11:10:02 +0200

    layout fixes

--- php/pieces/rocketleaguestatistics.php
+++ php/pieces/rocketleaguestatistics.php
@@ -11,7 +11,7 @@ $gamestatus = $listDAO->getStatus($gameID, $userID);
 ?>
 <h1>Rocket League</h1>
 <h2>Rang</h2>
-<div class=choice-wrapper>  
+<div class="choice-wrapper">  
     <?php foreach($rocketranks as $rank): ?>
         <label class="radiobutton-container"><?php echo $rank?>
             <input type="radio" name="rank" value='<?php echo $rank?>' <?php echo ($userrank == $rank)? 'checked' : ''?> required>
@@ -21,7 +21,7 @@ $gamestatus = $listDAO->getStatus($gameID, $userID);
 </div>
 <?php if(count($rocketroles)>0): ?>
     <h2>Rolle</h2>
-    <div class=choice-wrapper> 
+    <div class="choice-wrapper"> 
         <?php foreach($rocketroles as $role): ?>
             <label class="checkbox-container"><?php echo $role?>
                 <input type="checkbox" name="role[]" value='<?php echo $role?>' <?php echo (in_array($role, $userroles))? 'checked' : ''?>>
@@ -30,7 +30,9 @@ $gamestatus = $listDAO->getStatus($gameID, $userID);
         <?php endforeach; ?> 
     </div>
 <?php endif; ?>
-<label class="checkbox-container">Ich möchte, dass andere Spieler mich über dieses Spiel finden.
-        <input type="checkbox" name="visible" <?php echo ($gamestatus == 'active')? 'checked' : ''?>>
-        <span class="checkmark"></span>
-</label>
+<div class="visibility-wrapper">
+    <label class="checkbox-container">Ich möchte, dass andere Spieler mich über dieses Spiel finden.
+            <input type="checkbox" name="visible" <?php echo ($gamestatus == 'active')? 'checked' : ''?>>
+            <span class="checkmark"></span>
+    </label>
+</div>
