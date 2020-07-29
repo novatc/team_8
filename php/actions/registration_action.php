fa7193b1775d4662d4ecdae4d3c9a2a9019ecfb2
Lucas <lucas.wagner@uol.de>
2020-07-29 19:58:24 +0200

    Roboter Schutz mit IP und richtigem key

--- php/actions/registration_action.php
+++ php/actions/registration_action.php
@@ -3,26 +3,28 @@ require_once "session.php";
 updateSessionFromAction();
 $publicKey = "6LedxrcZAAAAACLlR3gcWywrkX6TSETOzoln7wAO";
 $url = "https://www.google.com/recaptcha/api/siteverify";
+$privatKey = "6LedxrcZAAAAAGeIYBp8yEzQf3DibxZmUiEUseol";
 
 if (empty($_SESSION['token'])) {
     $_SESSION['token'] = uniqid('', true);
 }
 
 
-if (array_key_exists('registersubmit', $_POST)){
+if (array_key_exists('registersubmit', $_POST)) {
     //echo "<pre>";print_r($_POST);echo "</pre>";
     $responseKey = $_POST['g-recaptcha-response'];
-    $response = file_get_contents($url.'?secret='.$publicKey.'&response='.$responseKey.'&remoteip='.$_SERVER['REMOTE_ADDR']);
+    $response = file_get_contents($url . '?secret=' . $privatKey . '&response=' . $responseKey . '&remoteip=' . $_SERVER['REMOTE_ADDR']);
     $response = json_decode($response);
     //echo "<pre>";print_r($response);echo "</pre>";
 
-    if ($response->success == 1){
+    if ($response->success == 1) {
         echo 'DU bis Mensch';
 
-    }
-    else{
+    } else {
         echo 'Teil der Roboterapokalypse';
-        echo "<pre>";print_r($response);echo "</pre>";
+        echo "<pre>";
+        print_r($response);
+        echo "</pre>";
 
         die();
     }
@@ -39,15 +41,15 @@ $required = array('username', 'usermail', 'password', 'passwordrepeat');
 
 
 /* Check if input field empty */
-foreach ($required as $field){
-    if (empty($_POST[$field])){
+foreach ($required as $field) {
+    if (empty($_POST[$field])) {
         $empty = true;
-        $message="Bitte alle Felder ausfüllen!";
+        $message = "Bitte alle Felder ausfüllen!";
         setcookie("registrationmessage", $message, 0, "/");
     }
 }
 
-if(!$empty){
+if (!$empty) {
     $username = $_POST['username'];
     $email = $_POST['usermail'];
     $pwd = $_POST['password'];
@@ -56,22 +58,22 @@ if(!$empty){
     $_SESSION['user'] = $username;
     $_SESSION['email'] = $email;
 
-    if(empty($_POST['acceptterms'])){
+    if (empty($_POST['acceptterms'])) {
         $error = true;
-        $message="Bitte akzeptieren Sie die Nutzungsbedingungen und die Datenschutzerklärung!";
+        $message = "Bitte akzeptieren Sie die Nutzungsbedingungen und die Datenschutzerklärung!";
         setcookie("registrationmessage", $message, 0, "/");
     }
-    
-    if(!$error){
+
+    if (!$error) {
         $userid = $userDAO->register($username, $email, $pwd, $pwdrepeat, 0);
-        if ($userid != -1){
+        if ($userid != -1) {
             $_SESSION['userid'] = $userid;
 
             header('Location: ../../playerprofile.php');
             exit();
-        } 
+        }
     }
-    
+
 }
 
 header('Location: ../../registration.php');
